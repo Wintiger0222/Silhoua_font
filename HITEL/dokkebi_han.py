2eac3fb051d972f9316090a2ebf1154cc31cbe30
wintiger0222 <0xodbs02@naver.com>
2023-10-17 16:02:59 +0900

    More Tool Update

--- HITEL/dokkebi_han.py
+++ HITEL/dokkebi_han.py
@@ -93,222 +93,240 @@ printf("	\"glyphs\": [\n")
 
 
 # 영어
+if ASCII_FONT != "!":
+    CP437_list = "\x00☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\x5C]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■\xA0"
+    fp = open(ASCII_FONT , 'rb')
+    fp.seek(start)
 
-CP437_list = "\x00☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\x5C]^_`abcdefghijklmnopqrstuvwxyz{|}~⌂ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■\xA0"
-fp = open(ASCII_FONT , 'rb')
-fp.seek(start)
-
-for i in range(len(CP437_list)):
-    printf("\t\t{\n")
-    if (i<0x20 or i>0x7f):
-        printf("\t\t\t\"unicode\": " + str(0xF800+i) + ",\n")
-    else:
-        printf("\t\t\t\"unicode\": " + str(ord(CP437_list[i])) + ",\n")
-    
-    #data print        
-    printf("\t\t\t\"data\": [\n")
-    for h in range(height):
-        printf("\t\t\t\t\"")
-        # horizon
-        for w in range(math.ceil(height/16)):
-            value = fp.read(1)[0]
-            bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
-            printf(bin_val)
-
-        if h < height-1:
-            printf("\",\n")
+    for i in range(len(CP437_list)):
+        printf("\t\t{\n")
+        if (i<0x20 or i>0x7f):
+            printf("\t\t\t\"unicode\": " + str(0xF800+i) + ",\n")
         else:
-            printf("\"\n")
+            printf("\t\t\t\"unicode\": " + str(ord(CP437_list[i])) + ",\n")
+        
+        #data print        
+        printf("\t\t\t\"data\": [\n")
+        for h in range(height):
+            printf("\t\t\t\t\"")
+            # horizon
+            for w in range(math.ceil(height/16)):
+                value = fp.read(1)[0]
+                bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
+                printf(bin_val)
 
-    printf("\t\t\t]\n")
-    printf("\t\t},\n")
-fp.close()
+            if h < height-1:
+                printf("\",\n")
+            else:
+                printf("\"\n")
+
+        printf("\t\t\t]\n")
+        printf("\t\t},\n")
+    fp.close()
 
 # 한글조합
-
-for i in range(44032,57363):
-    printf("\t\t{\n")
-    printf("\t\t\t\"unicode\": " + str(i) + ",\n")
-    printf("\t\t\t\"components\": ")
-    printf(str(UTF2CJJ(i))+"\n")
-    printf("\t\t},\n")
-
-
-# 한글자모
-jamo_list2=[57345, 57346, 57595, 57347, 57597, 57598, 57348, 57349, 57350, 57601, 57602, 57603, 57604, 57605, 57606, 57607, 57351, 57352, 57353, 57610, 57354, 57355, 57356, 57357, 57358, 57359, 57360, 57361, 57362, 57363, 57505, 57506, 57507, 57508, 57509, 57510, 57511, 57512, 57513, 57514, 57515, 57516, 57517, 57518, 57519, 57520, 57521, 57522, 57523, 57524, 57525]
-for i in range(12593,12643):
-    printf("\t\t{\n")
-    printf("\t\t\t\"unicode\": " + str(i) + ",\n")
-    printf("\t\t\t\"components\": [")
-    printf(str(jamo_list2[i-12593])+"],\n")
-    printf("\t\t\t\"advanceWidth\":16\n")
-    printf("\t\t},\n")
+if HAN_FONT != "!":
+    for i in range(44032,57363):
+        printf("\t\t{\n")
+        printf("\t\t\t\"unicode\": " + str(i) + ",\n")
+        printf("\t\t\t\"components\": ")
+        printf(str(UTF2CJJ(i))+"\n")
+        printf("\t\t},\n")
 
 
+    # 한글자모
+    jamo_list2=[57345, 57346, 57595, 57347, 57597, 57598, 57348, 57349, 57350, 57601, 57602, 57603, 57604, 57605, 57606, 57607, 57351, 57352, 57353, 57610, 57354, 57355, 57356, 57357, 57358, 57359, 57360, 57361, 57362, 57363, 57505, 57506, 57507, 57508, 57509, 57510, 57511, 57512, 57513, 57514, 57515, 57516, 57517, 57518, 57519, 57520, 57521, 57522, 57523, 57524, 57525]
+    for i in range(12593,12643):
+        printf("\t\t{\n")
+        printf("\t\t\t\"unicode\": " + str(i) + ",\n")
+        printf("\t\t\t\"components\": [")
+        printf(str(jamo_list2[i-12593])+"],\n")
+        printf("\t\t\t\"advanceWidth\":16\n")
+        printf("\t\t},\n")
 
-# 한글
 
-fp = open(HAN_FONT , 'rb')
-fp.seek(start)
-max_sybal=(max_jamo[0] * chosung_len) + (max_jamo[1] * jungsung_len) + (max_jamo[2] * jongsung_len)
-for i in range(max_sybal):
 
-    current = 0
-    if i < (max_jamo[0] * chosung_len):
-        current = 1
-    elif i < (max_jamo[0] * chosung_len) + (max_jamo[1] * jungsung_len):
-        current = 2
-    elif i < max_sybal:
-        current = 3
-    
-    printf("\t\t{\n")
-    printf("\t\t\t\"unicode\": " + str(i + PUA_start) + ",\n")
-    
-    #name print
-    printf("\t\t\t\"name\": \"DKB | ")
-    if current == 1:
-        temp = i
-        printf("choseong | ")
-    elif current == 2:
-        temp = i - (max_jamo[0] * chosung_len)
-        printf("jungseong | ")
-    elif current == 3:
-        temp = i - (max_jamo[0] * chosung_len) - (max_jamo[1] * jungsung_len)
-        printf("jongseong | ")
+    # 한글
 
-    if current != 0:
-        printf(str(temp//jamo_len[current-1] + 1) + " | " + jamo_list[current-1][temp%jamo_len[current-1]] + "\",\n")
-    else:
-        printf("\",\n")
-    
-    #data print        
-    printf("\t\t\t\"data\": [\n")
-    for h in range(height):
-        printf("\t\t\t\t\"")
-        # horizon
-        for w in range(math.ceil(width/8)):
-            value = fp.read(1)[0]
-            bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
-            printf(bin_val)
+    fp = open(HAN_FONT , 'rb')
+    fp.seek(start)
+    max_sybal=(max_jamo[0] * chosung_len) + (max_jamo[1] * jungsung_len) + (max_jamo[2] * jongsung_len)
+    for i in range(max_sybal):
 
-        if h < height-1:
-            printf("\",\n")
+        current = 0
+        if i < (max_jamo[0] * chosung_len):
+            current = 1
+        elif i < (max_jamo[0] * chosung_len) + (max_jamo[1] * jungsung_len):
+            current = 2
+        elif i < max_sybal:
+            current = 3
+        
+        printf("\t\t{\n")
+        printf("\t\t\t\"unicode\": " + str(i + PUA_start) + ",\n")
+        
+        #name print
+        printf("\t\t\t\"name\": \"DKB | ")
+        if current == 1:
+            temp = i
+            printf("choseong | ")
+        elif current == 2:
+            temp = i - (max_jamo[0] * chosung_len)
+            printf("jungseong | ")
+        elif current == 3:
+            temp = i - (max_jamo[0] * chosung_len) - (max_jamo[1] * jungsung_len)
+            printf("jongseong | ")
+
+        if current != 0:
+            printf(str(temp//jamo_len[current-1] + 1) + " | " + jamo_list[current-1][temp%jamo_len[current-1]] + "\",\n")
         else:
-            printf("\"\n")
+            printf("\",\n")
+        
+        #data print        
+        printf("\t\t\t\"data\": [\n")
+        for h in range(height):
+            printf("\t\t\t\t\"")
+            # horizon
+            for w in range(math.ceil(width/8)):
+                value = fp.read(1)[0]
+                bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
+                printf(bin_val)
 
-    printf("\t\t\t]\n")
-    printf("\t\t},\n")
-fp.close()
+            if h < height-1:
+                printf("\",\n")
+            else:
+                printf("\"\n")
+
+        printf("\t\t\t]\n")
+        printf("\t\t},\n")
+    fp.close()
 
 
 # KSG
+if KSG_FONT != "!":
+    KSG_list = "☺、。·‥…¨〃­―∥＼∼‘’“”〔〕〈〉《》「」『』【】±×÷≠≤≥∞∴°′″℃Å￠￡￥♂♀∠⊥⌒∂∇≡≒§※☆★○●◎◇◆□■△▲▽▼→←↑↓↔〓≪≫√∽∝∵∫∬∈∋⊆⊇⊂⊃∪∩∧∨￢⇒⇔∀∃´～ˇ˘˝˚˙¸˛¡¿ː∮∑∏¤℉‰◁◀▷▶♤♠♡♥♧♣⊙◈▣◐◑▤▥▨▧▦▩♨☏☎☜☞¶†‡↕↗↙↖↘♭♩♪♬㉿㈜№㏇™㏂㏘℡！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［￦］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝￣ﾡﾢﾣﾤﾥﾦﾧﾨﾩﾪﾫﾬﾭﾮﾯﾰﾱﾲﾳﾴﾵﾶﾷﾸﾹﾺﾻﾼﾽﾾￂￃￄￅￆￇￊￋￌￍￎￏￒￓￔￕￖￗￚￛￜㅤㅥㅦㅧㅨㅩㅪㅫㅬㅭㅮㅯㅰㅱㅲㅳㅴㅵㅶㅷㅸㅹㅺㅻㅼㅽㅾㅿㆀㆁㆂㆃㆄㆅㆆㆇㆈㆉㆊㆋㆌㆍㆎⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹ☺☺☺☺☺ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩ☺☺☺☺☺☺☺ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ☺☺☺☺☺☺☺☺αβγδεζηθικλμνξοπρστυφχψω☺☺☺☺☺☺─│┌┐┘└├┬┤┴┼━┃┏┓┛┗┣┳┫┻╋┠┯┨┷┿┝┰┥┸╂┒┑┚┙┖┕┎┍┞┟┡┢┦┧┩┪┭┮┱┲┵┶┹┺┽┾╀╁╃╄╅╆╇╈╉╊㎕㎖㎗ℓ㎘㏄㎣㎤㎥㎦㎙㎚㎛㎜㎝㎞㎟㎠㎡㎢㏊㎍㎎㎏㏏㎈㎉㏈㎧㎨㎰㎱㎲㎳㎴㎵㎶㎷㎸㎹㎀㎁㎂㎃㎄㎺㎻㎼㎽㎾㎿㎐㎑㎒㎓㎔Ω㏀㏁㎊㎋㎌㏖㏅㎭㎮㎯㏛㎩㎪㎫㎬㏝㏐㏓㏃㏉㏜㏆☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺ÆÐªĦ☺Ĳ☺ĿŁØŒºÞŦŊ☺㉠㉡㉢㉣㉤㉥㉦㉧㉨㉩㉪㉫㉬㉭㉮㉯㉰㉱㉲㉳㉴㉵㉶㉷㉸㉹㉺㉻ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮½⅓⅔¼¾⅛⅜⅝⅞æđðħıĳĸŀłøœßþŧŋŉ㈀㈁㈂㈃㈄㈅㈆㈇㈈㈉㈊㈋㈌㈍㈎㈏㈐㈑㈒㈓㈔㈕㈖㈗㈘㈙㈚㈛⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽⑾⑿⒀⒁⒂¹²³⁴ⁿ₁₂₃₄ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ☺☺☺☺☺☺☺☺☺☺☺АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ☺☺☺☺☺☺☺☺☺☺☺☺абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    fp = open(KSG_FONT , 'rb')
+    fp.seek(start)
 
-KSG_list = "☺、。·‥…¨〃­―∥＼∼‘’“”〔〕〈〉《》「」『』【】±×÷≠≤≥∞∴°′″℃Å￠￡￥♂♀∠⊥⌒∂∇≡≒§※☆★○●◎◇◆□■△▲▽▼→←↑↓↔〓≪≫√∽∝∵∫∬∈∋⊆⊇⊂⊃∪∩∧∨￢⇒⇔∀∃´～ˇ˘˝˚˙¸˛¡¿ː∮∑∏¤℉‰◁◀▷▶♤♠♡♥♧♣⊙◈▣◐◑▤▥▨▧▦▩♨☏☎☜☞¶†‡↕↗↙↖↘♭♩♪♬㉿㈜№㏇™㏂㏘℡！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［￦］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝￣ﾡﾢﾣﾤﾥﾦﾧﾨﾩﾪﾫﾬﾭﾮﾯﾰﾱﾲﾳﾴﾵﾶﾷﾸﾹﾺﾻﾼﾽﾾￂￃￄￅￆￇￊￋￌￍￎￏￒￓￔￕￖￗￚￛￜㅤㅥㅦㅧㅨㅩㅪㅫㅬㅭㅮㅯㅰㅱㅲㅳㅴㅵㅶㅷㅸㅹㅺㅻㅼㅽㅾㅿㆀㆁㆂㆃㆄㆅㆆㆇㆈㆉㆊㆋㆌㆍㆎⅰⅱⅲⅳⅴⅵⅶⅷⅸⅹ☺☺☺☺☺ⅠⅡⅢⅣⅤⅥⅦⅧⅨⅩ☺☺☺☺☺☺☺ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ☺☺☺☺☺☺☺☺αβγδεζηθικλμνξοπρστυφχψω☺☺☺☺☺☺─│┌┐┘└├┬┤┴┼━┃┏┓┛┗┣┳┫┻╋┠┯┨┷┿┝┰┥┸╂┒┑┚┙┖┕┎┍┞┟┡┢┦┧┩┪┭┮┱┲┵┶┹┺┽┾╀╁╃╄╅╆╇╈╉╊㎕㎖㎗ℓ㎘㏄㎣㎤㎥㎦㎙㎚㎛㎜㎝㎞㎟㎠㎡㎢㏊㎍㎎㎏㏏㎈㎉㏈㎧㎨㎰㎱㎲㎳㎴㎵㎶㎷㎸㎹㎀㎁㎂㎃㎄㎺㎻㎼㎽㎾㎿㎐㎑㎒㎓㎔Ω㏀㏁㎊㎋㎌㏖㏅㎭㎮㎯㏛㎩㎪㎫㎬㏝㏐㏓㏃㏉㏜㏆☺☺☺☺☺☺☺☺☺☺☺☺☺☺☺ÆÐªĦ☺Ĳ☺ĿŁØŒºÞŦŊ☺㉠㉡㉢㉣㉤㉥㉦㉧㉨㉩㉪㉫㉬㉭㉮㉯㉰㉱㉲㉳㉴㉵㉶㉷㉸㉹㉺㉻ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩ①②③④⑤⑥⑦⑧⑨⑩⑪⑫⑬⑭⑮½⅓⅔¼¾⅛⅜⅝⅞æđðħıĳĸŀłøœßþŧŋŉ㈀㈁㈂㈃㈄㈅㈆㈇㈈㈉㈊㈋㈌㈍㈎㈏㈐㈑㈒㈓㈔㈕㈖㈗㈘㈙㈚㈛⒜⒝⒞⒟⒠⒡⒢⒣⒤⒥⒦⒧⒨⒩⒪⒫⒬⒭⒮⒯⒰⒱⒲⒳⒴⒵⑴⑵⑶⑷⑸⑹⑺⑻⑼⑽⑾⑿⒀⒁⒂¹²³⁴ⁿ₁₂₃₄ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ☺☺☺☺☺☺☺☺☺☺☺АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ☺☺☺☺☺☺☺☺☺☺☺☺абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-fp = open(KSG_FONT , 'rb')
-fp.seek(start)
-
-for i in range(len(KSG_list)):
-    if(KSG_list[i] == '☺'):
+    for i in range(len(KSG_list)):
+        if(KSG_list[i] == '☺'):
+            for h in range(height):
+                for w in range(math.ceil(width/8)):
+                    fp.read(1)[0]
+            continue
+        printf("\t\t{\n")
+        printf("\t\t\t\"unicode\": " + str(ord(KSG_list[i])) + ",\n")
+        
+        #data print        
+        printf("\t\t\t\"data\": [\n")
         for h in range(height):
+            printf("\t\t\t\t\"")
+            # horizon
             for w in range(math.ceil(width/8)):
-                fp.read(1)[0]
-        continue
-    printf("\t\t{\n")
-    printf("\t\t\t\"unicode\": " + str(ord(KSG_list[i])) + ",\n")
-    
-    #data print        
-    printf("\t\t\t\"data\": [\n")
-    for h in range(height):
-        printf("\t\t\t\t\"")
-        # horizon
-        for w in range(math.ceil(width/8)):
-            value = fp.read(1)[0]
-            bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
-            printf(bin_val)
+                value = fp.read(1)[0]
+                bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
+                printf(bin_val)
 
-        if h < height-1:
-            printf("\",\n")
-        else:
-            printf("\"\n")
+            if h < height-1:
+                printf("\",\n")
+            else:
+                printf("\"\n")
 
-    printf("\t\t\t],\n")
-    printf("\t\t\t\"advanceWidth\":16\n")
-    printf("\t\t},\n")
-fp.close()
+        printf("\t\t\t],\n")
+        printf("\t\t\t\"advanceWidth\":16\n")
+        printf("\t\t},\n")
+    fp.close()
 
 # SPC
+if SPC_FONT != "!":
+    SPC_list = "!☺☻!♦!!•◘!◙!!!♫☼►◄!‼!!▬↨!!!!∟!!!!!!!!!!!!⌐¬!!!!!░▒▓!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!█▄▌▐▀!!!!!!!!!!!!!!!!!!!!⌠⌡!≈!∙!!!!!!"
+    fp = open(SPC_FONT , 'rb')
+    fp.seek(start)
 
-SPC_list = "!☺☻!♦!!•◘!◙!!!♫☼►◄!‼!!▬↨!!!!∟!!!!!!!!!!!!⌐¬!!!!!░▒▓!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!█▄▌▐▀!!!!!!!!!!!!!!!!!!!!⌠⌡!≈!∙!!!!!!"
-fp = open(SPC_FONT , 'rb')
-fp.seek(start)
-
-for i in range(len(SPC_list)):
-    if(SPC_list[i] == '!'):
+    for i in range(len(SPC_list)):
+        if(SPC_list[i] == '!'):
+            for h in range(height):
+                for w in range(math.ceil(width/8)):
+                    fp.read(1)[0]
+            continue
+        printf("\t\t{\n")
+        printf("\t\t\t\"unicode\": " + str(ord(SPC_list[i])) + ",\n")
+        
+        #data print        
+        printf("\t\t\t\"data\": [\n")
         for h in range(height):
+            printf("\t\t\t\t\"")
+            # horizon
             for w in range(math.ceil(width/8)):
-                fp.read(1)[0]
-        continue
-    printf("\t\t{\n")
-    printf("\t\t\t\"unicode\": " + str(ord(SPC_list[i])) + ",\n")
-    
-    #data print        
-    printf("\t\t\t\"data\": [\n")
-    for h in range(height):
-        printf("\t\t\t\t\"")
-        # horizon
-        for w in range(math.ceil(width/8)):
-            value = fp.read(1)[0]
-            bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
-            printf(bin_val)
+                value = fp.read(1)[0]
+                bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
+                printf(bin_val)
 
-        if h < height-1:
-            printf("\",\n")
-        else:
-            printf("\"\n")
+            if h < height-1:
+                printf("\",\n")
+            else:
+                printf("\"\n")
 
-    printf("\t\t\t],\n")
-    printf("\t\t\t\"advanceWidth\":16\n")
-    printf("\t\t},\n")
-fp.close()
-
-
-
-# SPC2
-
-
-fp = open(SPC_FONT , 'rb')
-fp.seek(start)
-
-for i in range(len(SPC_list)):
-    printf("\t\t{\n")
-    if (i<0x20):
-        printf("\t\t\t\"unicode\": " + str(0xF700+i) + ",\n")
-    else:
-        printf("\t\t\t\"unicode\": " + str(0xF780+i) + ",\n")
-    
-    #data print        
-    printf("\t\t\t\"data\": [\n")
-    for h in range(height):
-        printf("\t\t\t\t\"")
-        # horizon
-        for w in range(math.ceil(width/8)):
-            value = fp.read(1)[0]
-            bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
-            printf(bin_val)
-
-        if h < height-1:
-            printf("\",\n")
-        else:
-            printf("\"\n")
-
-    printf("\t\t\t],\n")
-    printf("\t\t\t\"advanceWidth\":16\n")
-    if(i<len(SPC_list)-1):
+        printf("\t\t\t],\n")
+        printf("\t\t\t\"advanceWidth\":16\n")
         printf("\t\t},\n")
-    else:
-        printf("\t\t}\n")
+    fp.close()
 
-fp.close()
 
+
+    # SPC2
+
+
+    fp = open(SPC_FONT , 'rb')
+    fp.seek(start)
+
+    for i in range(len(SPC_list)):
+        printf("\t\t{\n")
+        if (i<0x20):
+            printf("\t\t\t\"unicode\": " + str(0xF700+i) + ",\n")
+        else:
+            printf("\t\t\t\"unicode\": " + str(0xF780+i) + ",\n")
+        
+        #data print        
+        printf("\t\t\t\"data\": [\n")
+        for h in range(height):
+            printf("\t\t\t\t\"")
+            # horizon
+            for w in range(math.ceil(width/8)):
+                value = fp.read(1)[0]
+                bin_val = format(value, 'b').zfill(8).replace('1','#').replace('0','.')
+                printf(bin_val)
+
+            if h < height-1:
+                printf("\",\n")
+            else:
+                printf("\"\n")
+
+        printf("\t\t\t],\n")
+        printf("\t\t\t\"advanceWidth\":16\n")
+        printf("\t\t},\n")
+
+    fp.close()
+printf("		{\n")
+printf("			\"name\": \".notdef\",\n")
+printf("			\"unicode\": 0,\n")
+printf("			\"data\": [\n")
+printf("				\"################\",\n")
+printf("				\"##............##\",\n")
+printf("				\"#.#..........#.#\",\n")
+printf("				\"#..#........#..#\",\n")
+printf("				\"#...#......#...#\",\n")
+printf("				\"#....#....#....#\",\n")
+printf("				\"#.....#..#.....#\",\n")
+printf("				\"#......##......#\",\n")
+printf("				\"#......##......#\",\n")
+printf("				\"#.....#..#.....#\",\n")
+printf("				\"#....#....#....#\",\n")
+printf("				\"#...#......#...#\",\n")
+printf("				\"#..#........#..#\",\n")
+printf("				\"#.#..........#.#\",\n")
+printf("				\"##............##\",\n")
+printf("				\"################\"\n")
+printf("			]\n")
+printf("		}\n")
 printf("\t]\n")
 printf("}\n")
